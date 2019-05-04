5f5b921dcfe267f20694fdf0479124017f99b0b2
= <=>
2019-05-04 15:13:09 -0300

    Lab 12 versão final

--- lab12.py
+++ lab12.py
@@ -1,19 +1,22 @@
-# Este programa foi criado para interceptar as postagens feitas na rede social
-# FACESTAR e descobrir através do nosso informante infiltrado quais direções e elevações
+# Este programa foi criado para interceptar as postagens feitas na rede social FACESTAR.
+# Através de postagens do nosso informante infiltrado, podemos descobrir com o programa quais direções e elevações
 # dos nossos canhões devemos utilizar para atacarmos as bases da império antes que nos ataquem.
 # Entrada: Um texto de no máximo 1000 caracteres sendo a postagem no FACESTAR.
-# Saída: As direções e elevações de cada basa identificada no texto, uma em cada linha.
+# Saída: As direções e elevações de cada base identificada no texto, uma em cada linha.
 
 # Nome: Flavio Augusto Pereira Cunha
 # RA: 197083
 
-# post = input()
-post = 'GoRIla fUNeBREs ROTuLagem nEtUNo OraTorio vErmeLHo esquIva eMbATe traJetoriA EXTRaVIo uRANO cLIticO VERmeLhO ESPOrTIVO cRoCantE saBotAgem merCUriO pApEL vEntOSIdade INtEMPeSTiVo INStituCIonalIZaCao VErDe poLiPO conflITo ALVorocaMEnTo nEtUno desseRvICO fausTo EmBoneCADo AmARelO dIgNo PARTO NAvajO emPoLGAcAo imPerMeabiLIDaDE URanO ReconfORtaNtE TELUGu peNeirA AGlOMERaCao imPoSSiBILIdaDe veRde chiaDeIRa trIANGuLAr FANTasistA sAturNO diFIcIl Plangente nOCiVidade HORTO cRACk vErmelhO bASbaqUiCe EnroLADOr reElegEr ClINicAR CafEiCuLTOR teRrA coMovEr PoSsEs boTAo APAssivAr BLAsFEmaR VeRdE BuROCRATiZaCaO esTERcAr AFOBAdo AlUMINiFerO'
-post = post.lower()
+# Função principal que monta a saída com base na entrada
+def main():
+    # Entrada da postagem no FACESTAR
+    post = input()
+    post = post.lower() # ignorando maiúsculas ou minúsculas
 
-
-# Dicionário para os pares de valoes direção / elevação
-pairsFound = {}
+    directions = findDirections(post)
+    for direction in directions:
+        elevation = findElevation(direction['position'], post)
+        print(direction['name'] + ' - ' + elevation['name'])
 
 # Função que procura as palavras chave de direções no texto e retorna suas posições na ordem em que foram encontradas
 def findDirections(text):
@@ -36,33 +39,31 @@ def findDirections(text):
         while text.find(key, start) != -1:
             pos = text.find(key, start)
             item = {
-                'direction': direction,
+                'name': direction,
                 'position': pos
             }
             # E salva a posição encontrada junto com a direção correspondente
             positions.append(item)
             start = pos + 1
     # Ordena a lista de direções de acordo com a ordem que apareceram no texto
-    positions.sort(key= lambda pair: pair['position'])
+    positions.sort(key = lambda pair: pair['position'])
     return positions
 
-# Função que procura as palavras-chave de elevações a partir de uma posição no texto (no caso após a direção)
-def findElevations(pos):
+# Função que procura as palavras-chave de elevações e retorna a mais próxima a posição fornecida
+def findElevation(pos, text):
     # Dicionário palavras-chave > ângulos de elevação
     elevations = {
         'verde': '30',
         'amarelo': '45',
         'vermelho': '60'
     }
-
-    minorElevation = {'strPos': len(post), 'elevation': 0 }
-    for eleKey, elevation in elevations.items():
-        elePos = post.find(eleKey, dirPos)
+    nearElevation = {'position': len(text), 'elevation': 0 }
+    for key, elevation in elevations.items():
+        elePos = text.find(key, pos)
         if (elePos != -1):
-            if (elePos < minorElevation['strPos']):
-                minorElevation['strPos'] = elePos
-                minorElevation['elevation'] = elevation
+            if (elePos < nearElevation['position']):
+                nearElevation['position'] = elePos
+                nearElevation['name'] = elevation
+    return nearElevation
 
-# pairsFound[direction] = minorElevation['elevation']
-
-# print(pairsFound)
+main()
